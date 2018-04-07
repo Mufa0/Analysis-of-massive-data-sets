2a60d2da34412ca984a82e2a97958de542967cf7
Mufa0 <mateo.stjepanovic@gmail.com>
2018-04-07 14:42:24 +0200

    Added skeleton for SimHash

--- src/hr/fer/zemris/avsp/lab1/Main.java
+++ src/hr/fer/zemris/avsp/lab1/Main.java
@@ -3,7 +3,8 @@ package hr.fer.zemris.avsp.lab1;
 public class Main {
 
 	public static void main(String[] args) {
-
+		SimHash sh = new SimHash("fakultet elektrotehnike i racunarstva", false);
+		System.out.println(sh.calculateSimHash());
 	}
 
 }
--- src/hr/fer/zemris/avsp/lab1/SimHash.java
+++ src/hr/fer/zemris/avsp/lab1/SimHash.java
@@ -2,7 +2,17 @@
  * 
  */
 package hr.fer.zemris.avsp.lab1;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+
 import org.apache.commons.codec.digest.DigestUtils;
+
+import sun.security.provider.MD5;
+
 /**
  * @author Mateo Stjepanovic
  *
@@ -21,6 +31,60 @@ public class SimHash {
 		this.input = input;
 		this.isFilePath = isFilePath;
 	}
+	/**
+	 * Getter for input string
+	 * @return input string
+	 */
+	public String getInput() {
+		return input;
+	}
+	
+	/**
+	 * Setter for input string
+	 * @param input - input string
+	 */
+	public void setInput(String input) {
+		this.input = input;
+	}
+	
+	public String calculateSimHash() {
+		String[] units;
+		long[] sh = new long[128];
+		
+		if(this.isFilePath) {
+			extractText(this.input);
+		}
+		
+		Arrays.fill(sh,0);
+		units = this.input.split(" ");
+		
+		for (String unit : units) {
+			
+		}	
+		for ( int i = 0; i < 128; ++i) {
+			if(sh[i] >= 0) {
+				sh[i]= 1;
+			}else {
+				sh[i]= 0;
+			}
+		}
+		
+		return sh.toString();
+	}
+	
+	/**
+	 * Method that will read text document and store it back into input variable
+	 * @param filePath - file path to the text document
+	 */
+	private void extractText(String filePath) {
+		try {
+			this.input = new String(Files.readAllBytes(Paths.get(filePath)));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 	
 	
 	
